4a113af7e1d3fb176b395011d5bc06aebafb4262
Kevin Shen <kevinshen@berkeley.edu>
2017-01-27 02:47:16 -0800

    Set up Babel

---
+++ README.md
@@ -0,0 +1,6 @@
+#Oh Hell Scorekeeper
+Scorekeeping app for the Zazzle games lunch crew
+
+#Setup
+1. npm install
+2. firebase serve
---
+++ src/js/ohhell.jsx
@@ -0,0 +1,13 @@
+class MainMenu extends React.Component {
+        render() {
+          return (
+            <div className="main-menu">
+              <h1>Main Menu</h1>
+              <ul>
+                <li>New Game</li>
+                <li>Statistics</li>
+              </ul>
+            </div>
+          );
+        }
+      }
