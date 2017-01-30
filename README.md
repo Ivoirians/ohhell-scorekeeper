167445aacbeb43a843fca3866cd913838e24fc2d
Kevin Shen <kevinshen@berkeley.edu>
2017-01-30 02:36:09 -0800

    Start writing in Firebase interface

--- README.md
+++ README.md
@@ -3,5 +3,4 @@ Scorekeeping app for the Zazzle games lunch crew
 
 #Setup
 1. npm install
-2. npm run build
-3. firebase serve
+2. npm run dev
--- src/scripts/ohhell.jsx
+++ src/scripts/ohhell.jsx
@@ -1,5 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyChWYU04cxASlH_NL32nL7IOTs6YnnN_RI",
+  authDomain: "ohhellscorekeeper.firebaseapp.com",
+  databaseURL: "https://ohhellscorekeeper.firebaseio.com",
+  storageBucket: "ohhellscorekeeper.appspot.com",
+  messagingSenderId: "165596737293"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+
+
+
+
+
+
 
 var PageEnum = {
   MAIN_MENU: 1,
@@ -20,20 +39,23 @@ class App extends React.Component {
     });
   }
 
+  returnToMain() {
+    this.changePage(PageEnum.MAIN_MENU)
+  }
+
   render () {
     var partial;
-    console.log(this + " " + this.state + " " + this.state.currentPage);
     if (this.state.currentPage === PageEnum.MAIN_MENU)
     {
       partial = <MainMenu changePage={this.changePage.bind(this)} />;
     } 
     else if (this.state.currentPage === PageEnum.STATISTICS)
     {
-      partial = <Statistics changePage={this.changePage.bind(this)} />;
+      partial = <Statistics returnToMain={this.returnToMain.bind(this)} />;
     }
     else if (this.state.currentPage === PageEnum.NEW_GAME)
     {
-      partial = <NewGame changePage={this.changePage.bind(this)} />
+      partial = <Game returnToMain={this.returnToMain.bind(this)} />
     }
     else 
     {
@@ -48,25 +70,62 @@ class App extends React.Component {
   }
 }
 
+class CreateGame extends React.Component {
+
+}
+
+class Game extends React.Component {
+
+  createNewGame(players, scorekeeper, dealer) {
+    var gameMetaData = {
+      dtmCreated: new Date(),
+      players: players,
+      scorekeeper: scorekeeper,
+      dealer: dealer
+    }
+    this.state.currentGameKey = database.ref().child('games').push().key;
+
+    var updates = {};
+    updates['/games/' + this.state.currentGameKey] = gameMetaData;
+    for (var p in players){
+      updates['/user-games/' + p + "/" + this.state.currentGameKey] = gameMetaData; 
+    }
+
+    database.ref().update(updates);
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {scorekeeper: '', headerText: 'New Game'};
+  };
+
+
+
+  render() {
+    return (
+      <div className="new-game">
+        <h1>{this.state.headerText}</h1>
+        <form>
+          Scorekeeper: <input type="text" value={this.state.value}></input>
+        </form>
+        <button onClick={this.props.returnToMain}> Return to Main Menu </button>
+      </div>
+    );
+  }
+}
+
+
 
 class Statistics extends React.Component {
   render() {
     return (
       <div className="statistics">
+        <h1> Statistics </h1>
+        <button onClick={this.props.returnToMain}> Return to Main Menu </button>
       </div>
     );
   }
 }
-
-class NewGame extends React.Component {
-  render() {
-    return (
-      <div className="new-game">
-      </div>
-    );
-  }
-}
-
 
 class MainMenu extends React.Component {
 
@@ -74,22 +133,21 @@ class MainMenu extends React.Component {
     super(props);
   };
 
-  createNewGame(event) {
+  goToNewGame(event) {
     this.props.changePage(PageEnum.NEW_GAME);
   }
 
-  viewStatistics(event) {
+  goToStatistics(event) {
     this.props.changePage(PageEnum.STATISTICS);
   }
 
   render() {
-    console.log("Main Menu: " + this.state);
     return (
       <div className="main-menu">
         <h1>Main Menu</h1>
         <ul>
-          <button onClick={this.createNewGame.bind(this)}> New Game </button>
-          <button onClick={this.viewStatistics.bind(this)}> Statistics </button>
+          <button onClick={this.goToNewGame.bind(this)}> New Game </button>
+          <button onClick={this.goToStatistics.bind(this)}> Statistics </button>
         </ul>
       </div>
     );
