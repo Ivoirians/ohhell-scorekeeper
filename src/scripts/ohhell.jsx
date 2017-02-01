b1de3e2274f9d095c4a7a3ac9adace47711e1480
Kevin Shen <kevin.shen@admins-mbp-5.zazzle.com>
2017-01-31 20:06:03 -0800

    Add navigation between all views.
    Add css.

--- src/scripts/ohhell.jsx
+++ src/scripts/ohhell.jsx
@@ -13,7 +13,6 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-
 var PageEnum = {
   MAIN_MENU: 1,
   CREATE_GAME: 2,
@@ -23,17 +22,48 @@ var PageEnum = {
   STATISTICS: 6
 }
 
-
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {currentPage: PageEnum.MAIN_MENU};
+    this.state = {
+      currentPage: PageEnum.MAIN_MENU
+    };
   }
 
   changePage(newPage) {
     this.setState({
       currentPage: newPage
     });
+  }
+
+  getHeaderText() {
+    var headerText;
+    console.log(this.state.currentPage == PageEnum.MAIN_MENU);
+    switch(this.state.currentPage) {
+      case PageEnum.MAIN_MENU:
+        console.log("Header TEXT");
+        headerText = "Main Menu";
+        break;
+      case PageEnum.CREATE_GAME:
+        headerText = "Create Game";
+        break;
+      case PageEnum.ROUND_BIDS:
+        headerText = "Bids";
+        break;
+      case PageEnum.ROUND_TRICKS:
+        headerText = "Tricks";
+        break;
+      case PageEnum.WIN_SCREEN:
+        headerText = "Winner";
+        break;
+      case PageEnum.STATISTICS:
+        headerText = "Statistics";
+        break;
+      default:
+        headerText = "Main Menu";
+        break;
+    }
+    return headerText;
   }
 
   render () {
@@ -46,7 +76,7 @@ class App extends React.Component {
         partial = <CreateGame changePage={this.changePage.bind(this)} />;
         break;
       case PageEnum.ROUND_BIDS:
-        partial = <RoundBids changePage={this.changePage.bind(this)} />;
+        partial = <RoundBids roundNumber={1} changePage={this.changePage.bind(this)} />;
         break;
       case PageEnum.ROUND_TRICKS:
         partial = <RoundTricks changePage={this.changePage.bind(this)} />;
@@ -62,7 +92,8 @@ class App extends React.Component {
         break;
     }
     return (
-      <div>
+      <div id="app-container">
+        <h1 id="primary-header">{this.getHeaderText.bind(this)()}</h1>
         {partial}
       </div>
     );
@@ -86,7 +117,6 @@ class MainMenu extends React.Component {
   render() {
     return (
       <div className="main-menu">
-        <h1>Main Menu</h1>
         <ul>
           <button onClick={this.goToNewGame.bind(this)}> New Game </button>
           <button onClick={this.goToStatistics.bind(this)}> Statistics </button>
@@ -98,8 +128,12 @@ class MainMenu extends React.Component {
 
 class CreateGame extends React.Component {
 
-  returnToMain() {
-    this.props.changePage(PageEnum.MAIN_MENU)
+  goToMainMenu() {
+    this.props.changePage(PageEnum.MAIN_MENU);
+  }
+
+  goToRoundBids() {
+    this.props.changePage(PageEnum.ROUND_BIDS);
   }
 
   createNewGame(players, scorekeeper, dealer, date) {
@@ -125,7 +159,6 @@ class CreateGame extends React.Component {
     this.state = {scorekeeper: '', headerText: 'New Game', players: [], n:0};
   };
 
-
   addPlayer(playerNumber, playerName) {
     //check for duplicates, if people are going to be malicious--necessary?
     this.state.players[playerNumber] = {playerName: playerName, playerNumber:playerNumber};
@@ -134,7 +167,6 @@ class CreateGame extends React.Component {
   }
 
   render() {
-    console.log("Rendering form");
     var playerRows = this.state.players.map((player) =>
       <li key={player.playerNumber}>
         {player.playerName}
@@ -142,13 +174,13 @@ class CreateGame extends React.Component {
     );
     return (
       <div className="new-game">
-        <h1>{this.state.headerText}</h1>
          {playerRows}
         <h2>Players:</h2>
         <form>
           <AddPlayerRow playerNumber={1} addPlayer={this.addPlayer.bind(this)} />
         </form>
-        <button onClick={this.returnToMain.bind(this)}> Return to Main Menu </button>
+        <button onClick={this.goToRoundBids.bind(this)}> Start Round {this.props.roundNumber} </button>
+        <button onClick={this.goToMainMenu.bind(this)}> Return to Main Menu </button>
       </div>
     );
   }
@@ -178,9 +210,78 @@ class AddPlayerRow extends React.Component {
   }
 }
 
+class RoundBids extends React.Component {
+  
+  constructor(props) {
+    super(props);
+  }
 
+  goToRoundTricks() {
+    this.props.changePage(PageEnum.ROUND_TRICKS);
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.goToRoundTricks.bind(this)}> Finalize Bids </button>
+      </div>
+    );
+  }
+}
+
+class RoundTricks extends React.Component {
+  
+  constructor(props) {
+    super(props);
+  }
+
+  goToRoundBids() {
+    this.props.changePage(PageEnum.ROUND_BIDS);
+  }
+
+  goToWinScreen() {
+    this.props.changePage(PageEnum.WIN_SCREEN);
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.goToRoundBids.bind(this)}> End Round </button>
+        <button onClick={this.goToWinScreen.bind(this)}> End Game </button>
+      </div>
+    );
+  }
+}
+
+class WinScreen extends React.Component {
+  
+  constructor(props) {
+    super(props);
+  }
+
+  goToMainMenu() {
+    this.props.changePage(PageEnum.MAIN_MENU);
+  }
+
+  goToStatistics() {
+    this.props.changePage(PageEnum.STATISTICS);
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.goToMainMenu.bind(this)}> Back to Main Menu </button>
+        <button onClick={this.goToStatistics.bind(this)}> Statistics </button>
+      </div>
+    );
+  }
+}
 
 class Statistics extends React.Component {
+  
+  constructor(props) {
+    super(props);
+  }
 
   returnToMain() {
     this.props.changePage(PageEnum.MAIN_MENU)
@@ -189,13 +290,10 @@ class Statistics extends React.Component {
   render() {
     return (
       <div className="statistics">
-        <h1> Statistics </h1>
         <button onClick={this.returnToMain.bind(this)}> Return to Main Menu </button>
       </div>
     );
   }
 }
-
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
