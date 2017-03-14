387f00765e7e5b0871ebb124545c56c236d88743
Petar Ivanov <psivanov@gmail.com>
2017-03-14 00:42:29 -0700

    add statistics for hit % considering only rounds above 42

--- src/scripts/statistics.jsx
+++ src/scripts/statistics.jsx
@@ -35,10 +35,13 @@ class GamePlayers extends React.Component {
         //the rest
         game.players.forEach(player => {
           const name = player.playerName;
+          const stats = this.playerStats(game.state[name].bids, game.state[name].takes, game.state[name].scores, game.state.roundNumber);
           players[name] = players[name] || {};
           players[name].gameCount = (players[name].gameCount || 0) + 1;
-          players[name].roundCount = (players[name].roundCount || 0) + game.state[name].bids.filter(x => x !== '-').length;
-          players[name].hitCount = (players[name].hitCount || 0) + countArrayPrefix(game.state[name].bids, game.state[name].takes, game.state.roundNumber);          
+          players[name].roundCount = (players[name].roundCount || 0) + stats.roundCount;
+          players[name].hitCount = (players[name].hitCount || 0) + stats.hitCount;
+          players[name].above42RoundCount = (players[name].above42RoundCount || 0) + stats.above42RoundCount;
+          players[name].above42HitCount = (players[name].above42HitCount || 0) + stats.above42HitCount;                 
           //players[name].totalScore = (players[name].totalScore || 0) + player.currentScore;
         });
     });
@@ -54,6 +57,31 @@ class GamePlayers extends React.Component {
     });
   }
 
+  playerStats(bids, takes, scores, rounds) {
+    rounds = Math.min(rounds, bids.length);
+
+    const stats = {
+      roundCount: 0,
+      hitCount: 0,
+      above42RoundCount: 0,
+      above42HitCount: 0
+    }
+
+    for (var i = 0; i < rounds; i++) {
+      if (bids[i] !== '-') {
+        stats.roundCount++;
+        if(bids[i] === takes[i])
+          stats.hitCount++;
+        if(i > 0 && scores[i-1] >= 42){
+          stats.above42RoundCount++;
+          if(bids[i] === takes[i])
+            stats.above42HitCount++;
+        }
+      }
+    }
+    return stats;
+  }
+
   render() {
     const {players, sortOrder} = this.state;
 
@@ -63,6 +91,7 @@ class GamePlayers extends React.Component {
       case 'winpct': players.sort((a,b) => b.winCount / b.gameCount - a.winCount / a.gameCount); break;
       case 'winpctno42': players.sort((a,b) => b.winCountNo42 / b.gameCount - a.winCountNo42 / a.gameCount); break;      
       case 'hitpct': players.sort((a,b) => b.hitCount / b.roundCount - a.hitCount / a.roundCount); break;
+      case 'above42hitpct': players.sort((a,b) => b.above42HitCount / b.above42RoundCount - a.above42HitCount / a.above42RoundCount); break;
       case 'games': players.sort((a,b) => b.gameCount - a.gameCount); break;
     }
 
@@ -70,9 +99,10 @@ class GamePlayers extends React.Component {
       <tr key={player.name}>
         <td>{player.name}</td>
         <td>{player.winCount}</td>
-        <td>{Math.round(100 * player.winCount / player.gameCount)}</td>
-        <td>{Math.round(100 * player.winCountNo42 / player.gameCount)}</td>              
-        <td>{Math.round(100 * player.hitCount / player.roundCount)}</td>
+        <td>{(100 * player.winCount / player.gameCount).toFixed(1)}</td>
+        <td>{(100 * player.winCountNo42 / player.gameCount).toFixed(1)}</td>              
+        <td>{(100 * player.hitCount / player.roundCount).toFixed(1)}</td>
+        <td>{(100 * player.above42HitCount / player.above42RoundCount || 0).toFixed(1)}</td>        
         <td>{player.gameCount}</td>        
       </tr>);
     return (
@@ -83,7 +113,8 @@ class GamePlayers extends React.Component {
             <th className={sortOrder == 'wins' && 'selected'} onClick={()=>this.setState({sortOrder: 'wins'})}>Wins</th>
             <th className={sortOrder == 'winpct' && 'selected'} onClick={()=>this.setState({sortOrder: 'winpct'})}>Win %</th>
             <th className={sortOrder == 'winpctno42' && 'selected'} onClick={()=>this.setState({sortOrder: 'winpctno42'})}>No 42 Win %</th>            
-            <th className={sortOrder == 'hitpct' && 'selected'} onClick={()=>this.setState({sortOrder: 'hitpct'})}>Hit %</th>             
+            <th className={sortOrder == 'hitpct' && 'selected'} onClick={()=>this.setState({sortOrder: 'hitpct'})}>Hit %</th>    
+            <th className={sortOrder == 'above42hitpct' && 'selected'} onClick={()=>this.setState({sortOrder: 'above42hitpct'})}>> 42 Hit %</th>                       
             <th className={sortOrder == 'games' && 'selected'} onClick={()=>this.setState({sortOrder: 'games'})}>Games</th>
           </tr>
         </thead>
